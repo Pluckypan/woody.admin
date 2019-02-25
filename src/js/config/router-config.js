d4882b7864e57414988ac37b980f00b37c4bda30
plucky <py@meitu.com>
2019-02-25 13:05:24 +0800

    vue router base url

--- src/js/config/router-config.js
+++ src/js/config/router-config.js
@@ -3,69 +3,85 @@ import VueRouter from 'vue-router';
 import demoComponents from './demo-components';
 
 Vue.use(VueRouter);
+var isDev = process.env.NODE_ENV === 'development'
+console.log(isDev)
+var host = isDev ? "" : "http://www.echo.engineer/woody.admin"
 
 const initRouter = () => {
-  const routerParam = {
-    mode: 'history',
-    routes: [{
-      path: '/login',
-      name: 'login',
-      component: (resolve) => require(['components/login'], resolve)
-    }, {
-      path: '/',
-      component: (resolve) => require(['components/app/app-frame'], resolve),
-      children: [{
-        path: '',
-        name: 'Home',
-        component: (resolve) => require(['components/welcome/home'], resolve),
-        meta: {title: '首页', icon: 'icon-monitor'}
-      }, {
-        path: '/systemError',
-        name: 'systemError',
-        component: (resolve) => require(['components/error-pages/500'], resolve),
-        meta: {title: '系统错误'}
-      }, {
-        path: '/permissionError',
-        name: 'permissionError',
-        component: (resolve) => require(['components/error-pages/403'], resolve),
-        meta: {title: '权限错误'}
-      }, 
-      {
-        path: '/notfoundError',
-        name: 'notfoundError',
-        component: (resolve) => require(['components/error-pages/404'], resolve),
-        meta: {title: '页面找不到'}
-      },
-      ...demoComponents, 
-      {
-        path: '*',
-        component: (resolve) => require(['components/error-pages/404'], resolve),
-        meta: {title: '页面找不到'}
-      }]
-    }]
-  };
+	const routerParam = {
+		mode: 'history',
+		base: host,
+		routes: [{
+			path: '/login',
+			name: 'login',
+			component: (resolve) => require(['components/login'], resolve)
+		}, {
+			path: '/',
+			component: (resolve) => require(['components/app/app-frame'], resolve),
+			children: [{
+					path: '',
+					name: 'Home',
+					component: (resolve) => require(['components/welcome/home'], resolve),
+					meta: {
+						title: '首页',
+						icon: 'icon-monitor'
+					}
+				}, {
+					path: '/systemError',
+					name: 'systemError',
+					component: (resolve) => require(['components/error-pages/500'], resolve),
+					meta: {
+						title: '系统错误'
+					}
+				}, {
+					path: '/permissionError',
+					name: 'permissionError',
+					component: (resolve) => require(['components/error-pages/403'], resolve),
+					meta: {
+						title: '权限错误'
+					}
+				},
+				{
+					path: '/notfoundError',
+					name: 'notfoundError',
+					component: (resolve) => require(['components/error-pages/404'], resolve),
+					meta: {
+						title: '页面找不到'
+					}
+				},
+				...demoComponents,
+				{
+					path: '*',
+					component: (resolve) => require(['components/error-pages/404'], resolve),
+					meta: {
+						title: '页面找不到'
+					}
+				}
+			]
+		}]
+	};
 
-  let router = new VueRouter(routerParam);
+	let router = new VueRouter(routerParam);
 
-  router.beforeEach((to, from, next) => {
-    HeyUI.$LoadingBar.start();
-    if (to.meta && to.meta.title) {
-      document.title = to.meta.title + ' - 管理应用';
-    } else {
-      document.title = '管理系统';
-    }
-    next();
-  })
-  router.afterEach(() => {
-    HeyUI.$LoadingBar.success();
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
-    // baidu 统计，如果有自己的统计，请至index.html修改至自己的埋点
-    if( window._hmt) {
-      window._hmt.push(['_trackPageview', window.location.pathname]);
-    }
-  });
-  return router;
+	router.beforeEach((to, from, next) => {
+		HeyUI.$LoadingBar.start();
+		if (to.meta && to.meta.title) {
+			document.title = to.meta.title + ' - 管理应用';
+		} else {
+			document.title = '管理系统';
+		}
+		next();
+	})
+	router.afterEach(() => {
+		HeyUI.$LoadingBar.success();
+		document.documentElement.scrollTop = 0;
+		document.body.scrollTop = 0;
+		// baidu 统计，如果有自己的统计，请至index.html修改至自己的埋点
+		if (window._hmt) {
+			window._hmt.push(['_trackPageview', window.location.pathname]);
+		}
+	});
+	return router;
 }
 
 export default initRouter;
