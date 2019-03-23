c89935b37b6d0740627e6530231809c1e16c9382
plucky <plucky@echo.engineer>
2019-03-23 17:20:05 +0800

    add category editor

--- src/components/bookmarks/category.vue
+++ src/components/bookmarks/category.vue
@@ -1,51 +1,102 @@
-<style lang='less'>
-  .page-404-vue {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    margin-top: -200px;
-    margin-left: -150px;
-    height: 300px;
-    width: 300px;
-    text-align: center;
-    .background-image {
-      background: url(../../images/404.png);
-      height: 300px;
-      max-width: 500px;
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-      margin: 0 auto 50px;
-    }
-    p {
-      font-size: 22px;
-      color: #3788ee;
-    }
-  }
+<style lang="less">
+.basic-form-vue {
+	.h-panel-body {
+		padding-right: 60px;
+	}
+}
 </style>
+
 <template>
-  <div class="page-404-vue">
-    <div class="background-image"></div>
-    <p>Category~~</p>
-  </div>
+	<div class="app-form frame-page basic-form-vue">
+		<div class="h-panel">
+			<div class="h-panel-bar"><span class="h-panel-title">添加分类</span></div>
+			<div class="h-panel-body">
+				<Form
+					:label-width="110"
+					mode="twocolumn"
+					:model="data"
+					:rules="validationRules"
+					ref="form"
+					showErrorTip
+				>
+					<FormItem label="父级分类" prop="pid" single>
+						<Select v-model="data.pid" :datas="data.cats"></Select>
+					</FormItem>
+					<FormItem label="名称" prop="name">
+						<input
+							type="text"
+							v-model="data.name"
+							placeholder="限制输入10个字"
+							v-wordlimit="10"
+						/>
+					</FormItem>
+					<FormItem label="编号" readonly>{{ data.id }}</FormItem>
+					<FormItem label="排序" prop="order">
+						<Slider v-model="data.order" :range="{ start: 0, end: 255 }"></Slider>
+					</FormItem>
+					<FormItem readonly label="排序">{{ data.order }}</FormItem>
+					<FormItem label="日期" prop="create_time">
+						<DatePicker placeholder="选择日期" v-model="data.create_time"></DatePicker>
+					</FormItem>
+					<FormItem label="备注" prop="desc" single>
+						<textarea
+							rows="3"
+							v-autosize
+							v-wordcount="50"
+							placeholder="添加一段描述"
+							v-model="data.desc"
+						></textarea>
+					</FormItem>
+					<FormItem :no-padding="true">
+						<Button color="primary" :loading="isLoading" @click="submit">提交</Button>
+						&nbsp;&nbsp;&nbsp;
+						<Button @click="reset">重置</Button>
+					</FormItem>
+				</Form>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      
-    }
-  },
-  mounted() {
-    this.init()
-  },
-  methods: {
-    init() {
-      
-    }
-  },
-  computed: {
-    
-  }
-}
+	data() {
+		let that = this;
+		return {
+			mode: 'single',
+			data: {
+				pid: '0x000',
+				id: '0x001',
+				name: '常用',
+				desc: '',
+				order: 0,
+				create_time: Date.now(),
+				cats: [
+					{ title: '根', key: '0x000'},
+					{ title: 'Android', key: '0x001' },
+					{ title: 'iOS', key: '0x002' }
+				]
+			},
+			isLoading: false,
+			validationRules: {
+				required: ['pid', 'id', 'name', 'order', 'create_time']
+			}
+		};
+	},
+	methods: {
+		submit() {
+			let validResult = this.$refs.form.valid();
+			if (validResult.result) {
+				this.$Message('验证成功');
+				this.isLoading = true;
+				setTimeout(() => {
+					this.isLoading = false;
+				}, 1000);
+			}
+		},
+		reset() {
+			this.isLoading = false;
+			this.$refs.form.reset();
+		}
+	}
+};
 </script>
